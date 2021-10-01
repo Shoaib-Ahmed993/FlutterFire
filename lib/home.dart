f833b1ac3e1237a2aeac73798e1b70187445be35
Shoaib Ahmed <hussainshoaib46@yahoo.com>
2021-10-01 21:58:15 +0500

    having issues in post page using web

--- lib/home.dart
+++ lib/home.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'post.dart';
 
+// ignore: must_be_immutable
 class Home extends StatelessWidget {
+  Stream postStream = FirebaseFirestore.instance
+      .collection('posts')
+      .snapshots(includeMetadataChanges: true);
+
   @override
   Widget build(BuildContext context) {
-    void backToLogin() {
-      Navigator.of(context).pushNamed('/login');
-    }
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: SafeArea(
+              child: StreamBuilder<QuerySnapshot>(
+            stream: postStream,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Text('Something went wrong');
+              }
 
-    return Center(
-      child: Container(
-          child: Column(
-        children: [
-          Text("Home Page"),
-          SizedBox(height: 15,),
-          ElevatedButton(onPressed: backToLogin, child: Text("Back to Login"))
-        ],
-      )),
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text("Loading");
+              }
+
+              return ListView(
+                children: snapshot.data.docs.map((DocumentSnapshot document) {
+                  // ignore: unnecessary_cast
+                  Map data = document.data();
+                  return Post(data: data);
+                }).toList(),
+              );
+            },
+          )),
+        ),
+      ),
     );
   }
 }
---
+++ lib/post.dart
@@ -0,0 +1,35 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  
+    final Map data;
+    Post({this.data});
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.black,
+        width: 2,
+      )),
+      child: Column(
+        children: [
+          Image(
+            image: NetworkImage(
+              data['url'],
+            ),
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+          Text(data['title']),
+          Text(data['description']),
+        ],
+      ),
+    );
+  }
+}
